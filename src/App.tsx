bede7b96efa977d0ba033e4f556db2310136ddaf
Max Shabalin <shabalinmaksimromanovich@gmail.com>
2023-09-17 10:51:19 +0300

    styles structure and theme infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import {MainPageLazy} from "./components/pages/MainPage/MainPage.Lazy";
 import {AboutPageLazy} from "./components/pages/AboutPage/AboutPage.Lazy";
+import {useTheme} from "./theme/useTheme";
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
             <Link to={'/'}>Home</Link>
             <Link to={'/aboutPage'}>About</Link>
+            <button onClick={() => toggleTheme()}>TOGGLE</button>
             <Suspense fallback={<div> loading...</div>}>
                 <Routes>
                     <Route path={'/'} element={<MainPageLazy/>}/>
